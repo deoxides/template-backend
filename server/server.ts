391b46d5ec104345e9f7c4c6a8ab2154daeb58ef
sebastian mora <smora7933@gmail.com>
2022-07-07 21:17:12 -0400

    entorno de produccion

--- server/server.ts
+++ server/server.ts
@@ -50,7 +50,7 @@ class AplicationServer {
       saveUninitialized:false
     }))
     this.app.use(express.json());
-    this.app.use(express.static(path.resolve(__dirname,'../public')));
+    this.app.use(express.static('public'));
     this.app.use('/uploads', express.static('uploads'));
   }
 
